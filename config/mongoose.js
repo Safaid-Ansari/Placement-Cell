f3911d68e6c49e9767af700c5b181ff7f6eefc0d
Safaid-Ansari <safaidansari07@gmail.com>
2022-08-09 02:02:44 +0530

    Complete Placement Cell Project With Node JS

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+//Database Connection File
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Placement-Cell',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB:"));
+db.once('open', function(){
+    console.log('Connected to Database ');
+});
+
+
+module.exports = db;
